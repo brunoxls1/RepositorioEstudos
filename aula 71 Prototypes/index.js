e31145f779fa20601c7e2a1a1efa73094609616c
Brunospinola <brunospinola1@hotmail.com>
2021-10-12 21:22:27 -0300

    att

--- aula 71 Prototypes/index.js
+++ aula 71 Prototypes/index.js
@@ -1,2 +1,14 @@
 // Construtora -> molde (classe)
-Pessoa.prototype()
+// _proto_ = Pessoa.prototype
+function Pessoa(nome,sobrenome){
+    this.nome = nome,
+    this.sobrenome = sobrenome;
+   // this.nomeCompleto=()=>'ORIGINAL' + this.nome + ' ' + this.sobrenome;
+};
+Pessoa.prototype.nomeCompleto = function(){
+    return this.nome + ' '  + this.sobrenome;
+}
+
+const p1 = new Pessoa('bruno', 'pingola');
+const data = new Date();
+console.log(p1,data);
